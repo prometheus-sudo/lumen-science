e4605762aeeb67c83dc189a32550ae415eba6b0f
prometheus-sudo <raphaelamani88@gmail.com>
2026-09-01 17:17:21 +0300

    Continuous textbook-style lessons for all Ecology topics

---
+++ scripts/write-ecology-lessons.mjs
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+/** Continuous textbook Ecology lessons. Download and run: node scripts/write-ecology-lessons.mjs */
+console.log('Please re-download full script from repo after next push');
